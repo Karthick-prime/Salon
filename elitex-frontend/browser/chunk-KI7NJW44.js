a03dffe24681f27aeceb8a62d66e87114c4b3612
Karthick-prime <karthickgopi2002@gmail.com>
2025-12-22 21:31:22 +0530

    Auto-generated commit

---
+++ elitex-frontend/browser/chunk-KI7NJW44.js
@@ -0,0 +1,1 @@
+import"./chunk-4CLCTAJ7.js";var t=[{path:"",loadComponent:()=>import("./chunk-ZHB7DWBM.js").then(o=>o.DashboardComponent)}];export{t as USER_DASHBOARD_ROUTES};
---
+++ elitex-frontend/browser/chunk-NXOAHL6S.js
@@ -0,0 +1,1 @@
+import"./chunk-4CLCTAJ7.js";var n=[{path:"",loadComponent:()=>import("./chunk-RY2WTWHK.js").then(o=>o.AdminDashboardComponent)}];export{n as ADMIN_ROUTES};
